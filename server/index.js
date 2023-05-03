767048c2376a23c5e30032cab5b26cfd050913de
kylevelarde <52535645+TechGitMaster@users.noreply.github.com>
2023-05-03 15:07:46 +0800

    asdasds

--- server/index.js
+++ server/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
 const App = require('./App');
-/*const mongoose = require('mongoose');*/
+const mongoose = require('mongoose');
 
 //this is only for local___________________________________
 /*const cors = require('cors');
 app.use(cors());*/
 
 
-app.use(express.json())
+/*app.use(express.json())*/
 app.use('/', App)
 
 
-/*const database = async () => {
+const database = async () => {
     try{
         await mongoose.connect(process.env.DATABASE_URI, 
             { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,7 +20,7 @@ app.use('/', App)
         console.error(err);
     }
 }
-database();*/
+database();
 
 //this is only for local___________________________________
 /*mongoose.connection.once('open', () => { 
