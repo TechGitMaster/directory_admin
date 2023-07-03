809fa3f18640fe5d562fa826553c3623ed74a00a
kylevelarde <52535645+TechGitMaster@users.noreply.github.com>
2023-07-03 17:01:50 +0800

    change vercel

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const mongoose = require('mongoose');
 //1. This is just for second option if header in vercel.json have an error when uploading to vercel________________________
 //2. Also delete "headers" in vercel.json if have an error when uploading to vercel________________________
 //this is only for local___________________________________
-/*const cors = require('cors');
+const cors = require('cors');
 app.use(cors({
     origin: ['http://localhost:3000', 'https://thesisary-admin.vercel.app']
-}));*/
+}));
 
 // Set up options response for preflight requests
 //app.options('*', cors());
